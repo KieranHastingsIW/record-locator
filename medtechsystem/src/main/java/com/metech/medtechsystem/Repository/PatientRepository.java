8fea9c2d7aa1bc9c60f6777a9626a8a1dafcc3cd
KieranHastings <kieran.hastings@integration.works>
2023-01-27 08:29:43 +1300

    removing clutter

--- medtechsystem/src/main/java/com/metech/medtechsystem/Repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.metech.medtechsystem.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.metech.medtechsystem.modles.Patient;
-
-public interface PatientRepository extends JpaRepository<Patient, Long>{
-    
-}
